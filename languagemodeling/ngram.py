ae183cd90ddd0ebf5025493ba6293967cb4aa86f
Giovanni Rescia <giovannirescia@gmail.com>
2015-09-03 19:31:15 -0300

    se generan bien los modelos modelos entrenados

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from math import log2
 from random import random
 import operator
-import time
+
 
 class NGram(object):
 
@@ -18,11 +18,9 @@ class NGram(object):
         self.corpus = sents
         self.corpus_size = len(self.corpus)
 
-        if self.n > 1:
-            sents = list(map((lambda x: ['<s>']*(n-1) + x), sents))
 
-
-        sents = list(map((lambda x: x+['</s>']),sents))
+        sents = list(map((lambda x: ['<s>']*(n-1) + x), sents))
+        sents = list(map((lambda x: x + ['</s>']), sents))
 
         for sent in sents:
             for i in range(len(sent) - n + 1):
@@ -30,8 +28,6 @@ class NGram(object):
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
 
-
-    #default built-in prob, obsolete now...
     def prob(self, token, prev_tokens=None):
         n = self.n
         if not prev_tokens:
@@ -39,29 +35,26 @@ class NGram(object):
         assert len(prev_tokens) == n - 1
 
         tokens = prev_tokens + [token]
-        return self.counts[tuple(tokens)] / float(self.counts[tuple(prev_tokens)])
+        aux_count = self.counts[tuple(tokens)]
+        return aux_count / float(self.counts[tuple(prev_tokens)])
 
 
-    ###TODO###
-
+# ##TODO###
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
- 
         tokens -- the n-gram or (n-1)-gram tuple.
         """
         return self.counts[tokens]
 
-
     def cond_prob(self, token, prev_tokens=None):
         """Conditional probability of a token.
- 
         token -- the token.
         prev_tokens -- the previous n-1 tokens (optional only if n = 1).
         """
+
         if not prev_tokens:
             assert self.n == 1
             prev_tokens = tuple()
-
 
         hits = self.count((tuple(prev_tokens)+(token,)))
         sub_count = self.count(tuple(prev_tokens))
@@ -70,24 +63,21 @@ class NGram(object):
 
     def sent_prob(self, sent):
         """Probability of a sentence. Warning: subject to underflow problems.
- 
         sent -- the sentence as a list of tokens.
         """
 
         prob = 1.0
         sent = ['<s>']*(self.n-1)+sent+['</s>']
 
-        for i in range(self.n-1,len(sent)-self.n+1):
-            prob *= self.cond_prob(sent[i],tuple(sent[i-self.n+1:i]))
+        for i in range(self.n-1, len(sent)-self.n+1):
+            prob *= self.cond_prob(sent[i], tuple(sent[i-self.n+1:i]))
             if not prob:
                 break
 
         return prob
 
-
     def sent_log_prob(self, sent):
         """Log-probability of a sentence.
- 
         sent -- the sentence as a list of tokens.
         """
 
@@ -103,38 +93,52 @@ class NGramGenerator(object):
         """
         model -- n-gram model.
         """
+
         self.trained_model = model
+        self.probs = probs = dict()
+        self.sorted_probs = dict()
+
+        pre = [elem for elem in model.counts.keys() if not len(elem) == model.n]
+        suf = [elem for elem in model.counts.keys() if len(elem) == model.n]
+
+        for prefix in pre:
+            probs[prefix] = {sufix[-1]:model.cond_prob(sufix[-1],prefix) for sufix in suf if prefix==sufix[:-1]}
+
+        aux1 = list(probs.keys())
+
+        sp = [list(probs[x].items()) for x in aux1]
+
+        self.sorted_probs = {aux1[i]:sorted(sp[i]) for i in range(len(sp))}
 
 
     def generate_sent(self):
         """Randomly generate a sentence."""
+
         counter = defaultdict(int)
         sent = ''
 
-        for i in range(0,len(self.trained_model.corpus)):
-            counter[self.trained_model.corpus[i][0]]+=1
+        for i in range(0, len(self.trained_model.corpus)):
+            counter[self.trained_model.corpus[i][0]] += 1
 
-        return "hoal"
-
-        while not sent[-1]=='.':
-            sent+=" "+self.generate_token(tuple(sent.split()[:self.trained_model.n-1]))
+        while not sent[-1] == '</s>':
+            sent += " "+self.generate_token(tuple(sent.split()[:self.trained_model.n-1]))
 
         return sent
 
     def generate_token(self, prev_tokens=None):
         """Randomly generate a token, given prev_tokens.
- 
         prev_tokens -- the previous n-1 tokens (optional only if n = 1).
         """
+
         p = random()
+        xs = [elem[-1] for elem in self.probs.keys() if elem[0:len(prev_tokens)]==prev_tokens]
 
-            
-        xs = list(x for x in list(self.trained_model.counts.keys()) if len(x)==self.trained_model.n)
-        
-        ys = {}
-        ys = dict((elem[-1],self.trained_model.cond_prob(elem[-1],prev_tokens)) for elem in xs if self.trained_model.cond_prob(elem[-1],prev_tokens)>0)
+        ys = defaultdict(int)
+
+        for w in xs:
+            ys[w] = self.probs[prev_tokens+(w,)]
+
         ys = sorted(ys.items(),key=operator.itemgetter(1),reverse=True)
-
 
         acc = ys[0][1]
         for i in range(0,len(ys)):
@@ -148,38 +152,9 @@ class NGramGenerator(object):
 
 
 
-#LET'S DO SOME TESTING!
-
-#from nltk.corpus import PlaintextCorpusReader
 
 
-#sents = PlaintextCorpusReader('scripts/','shakespeare_no_signs.txt').sents()
-#print("\r\ngenerando modelo 1-grama\r\n")
-#t0 = time.time()
-#model1gram=NGram(1, sents)
-#print(time.time() - t0)
-#print("\r\ngenerando modelo 2-grama\r\n")
-#t0 = time.time()
-#model2gram=NGram(2, sents)
-#print(time.time() -t0)
-#print("\r\ngenerando modelo 3-grama\r\n")
-#t0=time.time()
-#model3gram=NGram(3, sents)
-#print(time.time() -t0)
-#print("\r\ngenerando modelo 4-grama\r\n")
-#t0 = time.time()
-#model4gram=NGram(4, sents)
-#print(time.time()-t0)
-
-
-#trained1gram=NGramGenerator(model1gram)
-#trained2gram=NGramGenerator(model2gram)
-#trained3gram=NGramGenerator(model3gram)
-#trained4gram=NGramGenerator(model4gram)
-
-#print("\r\nGeneración de tokens con modelo entrenado de 1-grama:\r\n")
-#fc = defaultdict(int)
-#for i in range(0,100):
-#    fc[trained1gram.generate_token()]+=1
-
-#print (fc)
+from nltk.corpus import PlaintextCorpusReader
+sents = PlaintextCorpusReader('scripts/','shakespeare.txt').sents()
+model2gram = NGram(2,sents[:15])
+trained2gram = NGramGenerator(model2gram)
