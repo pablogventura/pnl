fec21d8c36c2a7fa419efb3c028f3382e1bed414
Giovanni Rescia <giovannirescia@gmail.com>
2015-09-04 18:48:17 -0300

    terminado add_one; falta refactorizar el código, sigue ineficiente

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -25,6 +25,7 @@ class NGram(object):
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
 
+    # Obsolete now
     def prob(self, token, prev_tokens=None):
         n = self.n
         if not prev_tokens:
@@ -96,15 +97,16 @@ class NGramGenerator(object):
 
         pre = [elem for elem in model.counts.keys() if not len(elem) == model.n]
         suf = [elem for elem in model.counts.keys() if len(elem) == model.n]
-
+        # revisar, super ineficiente
         for prefix in pre:
             probs[prefix] = {sufix[-1]:model.cond_prob(sufix[-1],prefix) for sufix in suf if prefix==sufix[:-1]}
 
-        aux1 = list(probs.keys())
 
+        aux1 = list(probs.keys())
         sp = [list(probs[x].items()) for x in aux1]
 
-        self.sorted_probs = {aux1[i]:sorted(sp[i],key=lambda x: x[1],reverse=True) for i in range(len(sp))}
+        self.sorted_probs = {aux1[i]:sorted(sp[i],key=lambda x: (-x[1], x[0])) for i in range(len(sp))}
+
 
 
     def generate_sent(self):
@@ -140,11 +142,58 @@ class NGramGenerator(object):
 
 
 
+class AddOneNGram(object):
+
+    def __init__(self, n, sents):
+        """
+        n -- order of the model.
+        sents -- list of sentences, each one being a list of tokens.
+        """
+ 
+        assert n > 0
+        self.n = n
+        self.counts = counts = defaultdict(int)
+        self.voc = set()
+
+        sents = list(map((lambda x: x + ['</s>']), sents))
+
+        for s in sents:
+            self.voc = self.voc.union(set(s))
 
 
-#from nltk.corpus import PlaintextCorpusReader
-#sents = PlaintextCorpusReader('scripts/','shakespeare.txt').sents()
-#t0=time()
-#model4gram = NGram(4,sents)
-#trained4gram = NGramGenerator(model4gram)
-#print(time()-t0)
+        sents = list(map((lambda x: ['<s>']*(n-1) + x), sents))
+
+        for sent in sents:
+            for i in range(len(sent) - n + 1):
+                ngram = tuple(sent[i: i + n])
+                counts[ngram] += 1
+                counts[ngram[:-1]] += 1
+
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return self.counts[tokens]
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if not prev_tokens:
+            assert self.n == 1
+            prev_tokens = tuple()
+
+        hits = self.count((tuple(prev_tokens)+(token,)))
+        sub_count = self.count(tuple(prev_tokens))
+
+        return (hits+1) / (float(sub_count)+len(self.voc))
+ 
+
+
+    def V(self):
+        """Size of the vocabulary.
+        """
+        return len(self.voc)
