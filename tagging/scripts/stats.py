802c167ed8ba04a811f2be98afe8425bb2fe02f7
Giovanni Rescia <giovannirescia@gmail.com>
2015-09-28 18:38:23 -0300

    stats improved

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -19,32 +19,26 @@ if __name__ == '__main__':
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = corpus.tagged_sents()
     w_counts = defaultdict(int)
-    tag_counts = dict()
+    t_counts = defaultdict(int)
+    tag_dict = dict()
     a = time.time()
+
     for sent in sents:
         for s in sent:
-            aux_word = s[0]
+            word = s[0]
             tag = s[1]
-            w_counts[aux_word] += 1
-            if tag in tag_counts:
-                if aux_word in tag_counts[tag]:
-                    aux = tag_counts[aux_tag]
-                    tag_counts[tag] = {aux_word: list(aux.values())[0]+1}
+            w_counts[word] += 1
+            if tag in tag_dict:
+                if word in tag_dict[tag]:
+                    tag_dict[tag][word] += 1
                 else:
-                    sw = tag_counts[tag]
-                    tag_counts[tag] = {aux_word: 1}
-                    tag_counts[tag].update(sw)
+                    tag_dict[tag].update({word:1})
             else:
-                tag_counts[tag] = {aux_word: 1}
+                tag_dict[tag] = {word: 1}
 
     # compute the statistics
     print('sents: {}'.format(len(sents)))
     print('word counts: {}'.format(sum(w_counts.values())))
     print('vocabulary size: {}'.format(len(w_counts)))
-    print('tag vocabulary size: {}'.format(len(tag_counts)))
+    print('tag vocabulary size: {}'.format(len(tag_dict)))
     print('time: {}'.format(time.time()-a))
-    s = 0
-    for elem in list(tag_counts.values()):
-        s += sum(list(elem.values()))
-    print('cosa: {}'.format(s))
-    print('pepe: {}'.format(len(pepe)))
