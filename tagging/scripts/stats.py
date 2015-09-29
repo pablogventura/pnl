3b267c7a6830309485a552631cf01adabead0848
Giovanni Rescia <giovannirescia@gmail.com>
2015-09-28 23:27:43 -0300

    stats almost finished. only remains ambiguity.

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
             word = s[0]
             tag = s[1]
             w_counts[word] += 1
+            t_counts[tag] += 1
             if tag in tag_dict:
                 if word in tag_dict[tag]:
                     tag_dict[tag][word] += 1
@@ -35,10 +36,28 @@ if __name__ == '__main__':
                     tag_dict[tag].update({word:1})
             else:
                 tag_dict[tag] = {word: 1}
+    sorted_tags_words = dict()
+    for tag,dict_words in tag_dict.items():
+        sorted_tags_words[tag] = sorted(list(dict_words.items()),key=lambda x:-x[1])
 
+
+    sorted_tags = sorted(list(t_counts.items()),key=lambda x:-x[1])
     # compute the statistics
+    wrd_cnt = sum(w_counts.values())
     print('sents: {}'.format(len(sents)))
-    print('word counts: {}'.format(sum(w_counts.values())))
+    print('word counts: {}'.format(wrd_cnt))
     print('vocabulary size: {}'.format(len(w_counts)))
     print('tag vocabulary size: {}'.format(len(tag_dict)))
     print('time: {}'.format(time.time()-a))
+
+    print('\r\nlos 10 tags más frecuentes: ')
+    for i in range(10):
+        tag_tpl = sorted_tags[i]
+        tag = tag_tpl[0]
+        tag_frq = tag_tpl[1]
+        tag_prc = tag_frq / wrd_cnt
+        print('\r\nen el puesto número {0}, el tag `{1}` aparece un total de {2} veces; con un porcentaje total de {3}.\r\n'.format(i+1,tag,tag_frq,round(tag_prc,4)))
+        tagged_words = sorted_tags_words[tag][:5]
+        print('las 5 palabras más frecuentes con la etiqueta `{0}`\r\n'.format(tag))
+        for tw in tagged_words:
+            print('la palabra `{0}` con una frecuencia de {1}'.format(tw[0],tw[1]))
