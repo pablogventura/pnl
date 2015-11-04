2600940f57d4517f438699917246c9ed85358594
Giovanni Rescia <giovannirescia@gmail.com>
2015-11-04 19:06:56 -0300

    cky almost finished, remains backpointer

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -5,71 +5,68 @@ class CKYParser:
         grammar -- a binarised NLTK PCFG.
         """
         self.productions = grammar.productions()
-        # sent
-        self.lexicals = [elem for elem in self.productions if elem.is_lexical()]
-        self.d = dict()
-        for elem in self.lexicals:
-            w = elem.rhs()[0]
-            pos = str(elem.lhs())
-            self.d[(w,pos)] = elem.logprob()
 
-        self.non_lexicals = [elem for elem in self.productions if elem.is_nonlexical()]
         self.q_dict = {}
-        for elem in self.non_lexicals:
-            self.q_dict[elem.lhs()] = {elem.rhs() : elem.logprob()}
+        for elem in self.productions:
+            lhs = elem.lhs()
+            rhs = elem.rhs()
+            lp = elem.logprob()
+            if lhs in self.q_dict:
+                self.q_dict[lhs].update({rhs : lp})
+            else:
+                self.q_dict[elem.lhs()] = {elem.rhs() : elem.logprob()}
+
         # set of Non terminals
-        self.N = {elem.lhs() for elem in self.non_lexicals}
+        self.lex_N = {elem.lhs() for elem in self.productions if elem.is_lexical()}
+        self.non_lex_N = {elem.lhs() for elem in self.productions if elem.is_nonlexical()}
 
     def parse(self, sent):
         """Parse a sequence of terminals. 
         sent -- the sequence of terminals.
         """
 
-        self._pi = {}
-        # X -> a productions
-        lexicals = self.lexicals
         productions = self.productions
-        d = self.d
-        N = self.N
-        n = len(sent)
-        lex_set = {str(elem.lhs()) for elem in lexicals}
         q_dict = self.q_dict
+        lex_N = self.lex_N
+        non_lex_N = self.non_lex_N
+
+        n = len(sent)
+        self._pi = {}
 
         # INIT
         for i in range(1, n + 1):
-            self._pi[(i, i)] = {}
+            for j in range(1, n + 1):
+                if j > i:
+                    self._pi[(i, j)] = {}
+
+        for i in range(1, n + 1):
             w = sent[i - 1]
-            for nt in lex_set:
-                if (w, nt) in d:
-                    self._pi[(i, i)] = {nt : d[(w, nt)]}
+            for nt in lex_N:
+                if (w,) in q_dict[nt]:
+                    lp = q_dict[nt][(w,)]
+                    self._pi[(i, i)] = {nt : lp}
 
-        for k in range(1, n + 1):
-            for m in range(1, n + 1):
-                if not k == m:
-                    self._pi[(k, m)] = {}
-
-
-
-        print(self._pi)
         # RECURSIVE CASE
         for l in range(1, n):
             for i in range(1, n - l + 1):
                 j = i + l
-                for X in N:
+                for X in non_lex_N:
                     # all productions to compare
                     prod_xs = [elem for elem in productions if elem.lhs() == X]
                     for prd in prod_xs:
                         Y = prd.rhs()[0]
                         Z = prd.rhs()[1]
-
-                        if X in q_dict:
-                            if (Y, Z) in q_dict[X]:
-                                aux_q = q_dict[X][(Y, Z)]
-
-                                for s in range(i, j - 2):
-                                    ys.append(aux_q + self._pi(i, s, Y) + self._pi(s + 1, j, Z))
-                                    print(ys)
-                mx = max(ys)
-                self._pi[(i, j)] = {X : mx}
-
-        print(self._pi)
+                        aux_q = q_dict[X][(Y, Z)]
+                        ys = []
+                        for s in range(i, j):
+                            if Y in self._pi[(i, s)]:
+                                if Z in self._pi[(s + 1, j)]:
+                                    ys.append((X, (aux_q + self._pi[(i, s,)][Y] + self._pi[(s + 1, j,)][Z])))
+                        if ys:
+                            max_tpl = max(ys)
+                            nt = max_tpl[0]
+                            lp = max_tpl[1]
+                            self._pi[(i, j)] = {nt : lp}
+                            print('')
+                            print(self._pi)
+                            print('')
