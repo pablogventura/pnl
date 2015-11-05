4687eea43250892f14df1d92e59e0af9eb0d3e42
Giovanni Rescia <giovannirescia@gmail.com>
2015-11-04 21:22:48 -0300

    backpointer almost finished

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -1,3 +1,7 @@
+from nltk.grammar import Nonterminal
+from nltk import Tree
+
+
 class CKYParser:
  
     def __init__(self, grammar):
@@ -5,20 +9,34 @@ class CKYParser:
         grammar -- a binarised NLTK PCFG.
         """
         self.productions = grammar.productions()
+        self.lex_N = {str(elem.lhs()) for elem in self.productions if elem.is_lexical()}
+        self.non_lex_N = {str(elem.lhs()) for elem in self.productions if elem.is_nonlexical()}
 
-        self.q_dict = {}
-        for elem in self.productions:
-            lhs = elem.lhs()
+        non_lx = [e for e in self.productions if e.is_nonlexical()]
+        lx = [e for e in self.productions if e.is_lexical()]
+
+        # dict of X -> a productions with its probabilities
+        self.q_non_lx_dict = {}
+        for elem in non_lx:
+            lhs = str(elem.lhs())
+            rhs = (str(elem.rhs()[0]), str(elem.rhs()[1]))
+            lp = elem.logprob()
+            if lhs in self.q_non_lx_dict:
+                self.q_non_lx_dict[lhs].update({rhs : lp})
+            else:
+                self.q_non_lx_dict[lhs] = {rhs : lp}
+
+        # dict of X -> Y Z productions with its probabilities
+        self.q_lx_dict = {}
+        for elem in lx:
+            lhs = str(elem.lhs())
             rhs = elem.rhs()
             lp = elem.logprob()
-            if lhs in self.q_dict:
-                self.q_dict[lhs].update({rhs : lp})
+            if lhs in self.q_lx_dict:
+                self.q_lx_dict[lhs].update({rhs : lp})
             else:
-                self.q_dict[elem.lhs()] = {elem.rhs() : elem.logprob()}
+                self.q_lx_dict[lhs] = {rhs : lp}
 
-        # set of Non terminals
-        self.lex_N = {elem.lhs() for elem in self.productions if elem.is_lexical()}
-        self.non_lex_N = {elem.lhs() for elem in self.productions if elem.is_nonlexical()}
 
     def parse(self, sent):
         """Parse a sequence of terminals. 
@@ -26,25 +44,30 @@ class CKYParser:
         """
 
         productions = self.productions
-        q_dict = self.q_dict
+        q_lx_dict = self.q_lx_dict
+        q_non_lx_dict = self.q_non_lx_dict
         lex_N = self.lex_N
         non_lex_N = self.non_lex_N
 
         n = len(sent)
         self._pi = {}
+        self._bp = {}
 
         # INIT
+        # X -> Y Z productions
         for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if j > i:
-                    self._pi[(i, j)] = {}
+            for j in range(i + 1, n + 1):
+                self._pi[(i, j)] = {}
+                self._bp[(i, j)] = {}
 
+        # X -> a productions
         for i in range(1, n + 1):
             w = sent[i - 1]
             for nt in lex_N:
-                if (w,) in q_dict[nt]:
-                    lp = q_dict[nt][(w,)]
+                if (w,) in q_lx_dict[nt]:
+                    lp = q_lx_dict[nt][(w,)]
                     self._pi[(i, i)] = {nt : lp}
+                    self._bp[(i, i)] = {nt : Tree(nt, [w])}
 
         # RECURSIVE CASE
         for l in range(1, n):
@@ -52,21 +75,36 @@ class CKYParser:
                 j = i + l
                 for X in non_lex_N:
                     # all productions to compare
-                    prod_xs = [elem for elem in productions if elem.lhs() == X]
+                    prod_xs = [elem for elem in productions if str(elem.lhs()) == X]
                     for prd in prod_xs:
-                        Y = prd.rhs()[0]
-                        Z = prd.rhs()[1]
-                        aux_q = q_dict[X][(Y, Z)]
+                        Y = str(prd.rhs()[0])
+                        Z = str(prd.rhs()[1])
+                        aux_lp = q_non_lx_dict[X][(Y, Z)]
                         ys = []
                         for s in range(i, j):
                             if Y in self._pi[(i, s)]:
                                 if Z in self._pi[(s + 1, j)]:
-                                    ys.append((X, (aux_q + self._pi[(i, s,)][Y] + self._pi[(s + 1, j,)][Z])))
+                                    ys.append((X, (aux_lp + self._pi[(i, s,)][Y] + self._pi[(s + 1, j,)][Z])))
                         if ys:
                             max_tpl = max(ys)
                             nt = max_tpl[0]
                             lp = max_tpl[1]
                             self._pi[(i, j)] = {nt : lp}
+                            # backpointer
+                            xs_tree = []
+                            for k in range(i, j + 1):
+                                for t in range(k + 1, j + 1):
+                                    if self._bp[(k, t)]:
+                                        aux = list(self._bp[(k, t)].values())[0]
+                                        xs_tree.append((aux,[]))
                             print('')
-                            print(self._pi)
+                            print(xs_tree)
                             print('')
+                            right_tree = xs_tree[0]
+                            left_tree = xs_tree[1]
+                            self._bp[(i, j)] = {nt : Tree(nt,[right_tree, left_tree])}
+
+        print('')
+#        print(self._bp)
+        print('')
+        return (self._pi[(1, n)]['S'], None)
