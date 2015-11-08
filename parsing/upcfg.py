83c7afecabd81fad018bd5bae282da7d7949860a
Giovanni Rescia <giovannirescia@gmail.com>
2015-11-08 20:07:52 -0300

    parse method left

--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from nltk.grammar import Production as P, ProbabilisticProduction as PP
+
+
 class UPCFG:
     """Unlexicalized PCFG.
     """
@@ -7,9 +11,42 @@ class UPCFG:
         parsed_sents -- list of training trees.
         """
 
+        # list of all productions in training trees
+        prods = []
+        for tree in parsed_sents:
+            productions = tree.productions()
+            for e in productions:
+                lhs = e.lhs()
+                rhs = e.rhs()
+                if e.is_lexical():
+                    p = P(lhs, [str(lhs)])
+                    prods.append(p)
+                else:
+                    a1, a2 = rhs
+                    prods.append(P(lhs, [a1,a2]))
+
+        self.counts = counts = defaultdict(int)
+        for prd in prods:
+            lhs = prd.lhs()
+            rhs = prd.rhs()
+            counts[lhs] += 1
+            counts[rhs] += 1
+
+        self.upgfs = []
+        for prod in prods:
+            l = prod.lhs()
+            r = prod.rhs()
+            p = counts[r] / counts[l]
+            if prod.is_lexical():
+                up = PP(l,[r[0]], prob=p)
+            else:
+                up = PP(l, [r[0],r[1]], prob=p)
+            self.upgfs.append(up)
+
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
+        return self.upgfs
 
     def parse(self, tagged_sent):
         """Parse a tagged sentence.
